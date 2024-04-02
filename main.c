ff22ff0467382bba7f6a1babe94045ecfebf4844
Altois0 <145287667+Altois0@users.noreply.github.com>
2024-04-02 18:03:54 -0400

    bugfix + 2d main.c
    added a temporary fix to the issue that prints the screen twice, added 2 dimensions (3 rows)

--- main.c
+++ main.c
@@ -6,8 +6,7 @@ int main(void) {
   char direction = 0;
   while (repeatMovement == 0) {
 
-    // input detection
-    printf("ready");
+    // input
     scanf("%c", &direction);
     switch (direction) {
     case 'a':
@@ -17,60 +16,147 @@ int main(void) {
     case 'd':
       position++;
       break;
+
+    case 'w':
+      position = position + 10;
+      break;
+
+    case 's':
+      position = position - 10;
+      break;
+
     case 'n':
       position = position;
 
     default:
-      printf("error");
+      printf("\n");
       break;
     }
 
-    // print
+    // printr
     switch (position) {
     case (1):
-      printf("1000000000");
+      printf("0000000000\n0000000000\n1000000000\n\n");
       break;
 
     case (2):
-      printf("0100000000");
+      printf("0000000000\n0000000000\n0100000000\n\n");
       break;
 
     case (3):
-      printf("0010000000");
+      printf("0000000000\n0000000000\n0010000000\n\n");
       break;
 
     case (4):
-      printf("0001000000");
+      printf("0000000000\n0000000000\n0001000000\n\n");
       break;
 
     case (5):
-      printf("0000100000");
+      printf("0000000000\n0000000000\n0000100000\n\n");
       break;
 
     case (6):
-      printf("0000010000");
+      printf("0000000000\n0000000000\n0000010000\n\n");
       break;
 
     case (7):
-      printf("0000001000");
+      printf("0000000000\n0000000000\n0000001000\n\n");
       break;
 
     case (8):
-      printf("0000000100");
+      printf("0000000000\n0000000000\n0000000100\n\n");
       break;
 
     case (9):
-      printf("0000000010");
+      printf("0000000000\n0000000000\n0000000010\n\n");
       break;
 
     case (10):
-      printf("0000000001");
+      printf("0000000000\n0000000000\n0000000001\n\n");
+      break;
+
+    case (11):
+      printf("0000000000\n1000000000\n0000000000\n\n");
+      break;
+    case (12):
+      printf("0000000000\n0100000000\n0000000000\n\n");
+      break;
+
+    case (13):
+      printf("0000000000\n0010000000\n0000000000\n\n");
+      break;
+
+    case (14):
+      printf("0000000000\n0001000000\n0000000000\n\n");
+      break;
+
+    case (15):
+      printf("0000000000\n0000100000\n0000000000\n\n");
+      break;
+
+    case (16):
+      printf("0000000000\n0000010000\n0000000000\n\n");
+      break;
+
+    case (17):
+      printf("0000000000\n0000001000\n0000000000\n\n");
+      break;
+
+    case (18):
+      printf("0000000000\n0000000100\n0000000000\n\n");
+      break;
+
+    case (19):
+      printf("0000000000\n0000000010\n0000000000\n\n");
+      break;
+
+    case (20):
+      printf("1000000000\n0000000000\n0000000000\n\n");
+      break;
+
+    case (21):
+      printf("0100000000\n0000000000\n0000000000\n\n");
+      break;
+
+    case (22):
+      printf("0010000000\n0000000000\n0000000000\n\n");
+      break;
+
+    case (23):
+      printf("0001000000\n0000000000\n0000000000\n\n");
+      break;
+
+    case (24):
+      printf("00001000000\n0000000000\n0000000000\n\n");
+      break;
+
+    case (25):
+      printf("0000010000\n0000000000\n0000000000\n\n");
+      break;
+
+    case (26):
+      printf("0000001000\n0000000000\n0000000000\n\n");
+      break;
+
+    case (27):
+      printf("0000000100\n0000000000\n0000000000\n\n");
+      break;
+
+    case (28):
+      printf("0000000010\n0000000000\n0000000000\n\n");
+      break;
+
+    case (29):
+      printf("0000000001\n0000000000\n0000000000\n\n");
+      break;
+
+    case (30):
+      printf("0000000001\n0000000000\n0000000000\n\n");
       break;
 
     default:
-      printf("error");
+      printf("\n");
       break;
     }
-    repeatMovement = 0;
   }
 }
